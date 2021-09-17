720172ba574186bc81f8f07917bc97a0a5701124
nidges <nidges@yandex.ru>
2021-09-17 17:46:25 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .helpOption('-h, --help', 'output usage information');
+    // .version('0.0.1')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format');
+
 
 program.parse();
