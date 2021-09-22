4aca20491234b8584d37cf20852b9f4dab1eae65
nidges <nidges@yandex.ru>
2021-09-22 16:24:23 +0300

    badges

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 ### genDiff - two json files, pretty view
 [![asciicast](https://asciinema.org/a/NNS08MaudOYBwbw0YsXet4v0M.svg)](https://asciinema.org/a/NNS08MaudOYBwbw0YsXet4v0M)
+
+### CodeClimate 
+Maintainability:
+[![Maintainability](https://api.codeclimate.com/v1/badges/70f28cf06c690228a521/maintainability)](https://codeclimate.com/github/nidges/backend-project-lvl2/maintainability)
+Test Coverage:
+[![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/nidges/backend-project-lvl2/test_coverage)
--- src/index.js
+++ src/index.js
@@ -3,60 +3,58 @@ import { extname, resolve } from 'path';
 import { cwd } from 'process';
 import _ from 'lodash';
 
-// const path1 = '/Users/mariastepanova/WebstormProjects/backend-project-lvl2/__fixtures__/file1.json';
+// const path1 =
+// '/Users/mariastepanova/WebstormProjects/backend-project-lvl2/__fixtures__/file1.json';
 // const path2 = '../__fixtures__/file2.json';
-// const path2 = '/Users/mariastepanova/WebstormProjects/backend-project-lvl2/__fixtures__/file2.json';
+// const path2 =
+// '/Users/mariastepanova/WebstormProjects/backend-project-lvl2/__fixtures__/file2.json';
 
 const genDiff = (path1, path2) => {
-    const ext1 = extname(path1);
-    const ext2 = extname(path2);
+  // const ext1 = extname(path1);
+  // const ext2 = extname(path2);
 
-    if (!path1.startsWith('/')) {
-        path1 = resolve(cwd(), path1);
+  const path1Resolved = path1.startsWith('/') ? path1 : resolve(cwd(), path1);
+  const path2Resolved = path2.startsWith('/') ? path2 : resolve(cwd(), path2);
+
+  const json1 = readFileSync(path1Resolved, 'utf8');
+  const json2 = readFileSync(path2Resolved, 'utf8');
+
+  const obj1 = JSON.parse(json1);
+  const obj2 = JSON.parse(json2);
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  const allKeys = _.union(keys1, keys2);
+
+  const allKeysSorted = _.sortBy(allKeys);
+
+  const string = allKeysSorted.reduce((acc, key) => {
+    if (!_.has(obj1, key)) {
+      // console.log('----> if key is only in second',
+      // `${acc}\n  + ${key} : ${obj2[key]}`)
+      return `${acc}\n  + ${key} : ${obj2[key]}`;
     }
 
-    if (!path2.startsWith('/')) {
-        path2 = resolve(cwd(), path2);
+    if (!_.has(obj2, key)) {
+      // console.log('----> if key is only in first',
+      // `${acc}\n  - ${key} : ${obj1[key]}`)
+      return `${acc}\n  - ${key} : ${obj1[key]}`;
     }
 
-    const json1 = readFileSync(path1, 'utf8');
-    const json2 = readFileSync(path2, 'utf8');
+    if (obj1[key] !== obj2[key]) {
+      // console.log('----> if keys are not equal',
+      // `${acc}\n  - ${key} : ${obj1[key]} \n  + ${key} : ${obj2[key]}`)
+      return `${acc}\n  - ${key} : ${obj1[key]} \n  + ${key} : ${obj2[key]}`;
+    }
 
-    const obj1 = JSON.parse(json1);
-    const obj2 = JSON.parse(json2);
+    // console.log('----> if keys are equal',
+    // `${acc}\n    ${key} : ${obj1[key]}`)
+    return `${acc}\n    ${key} : ${obj1[key]}`;
+  }, '');
 
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-
-    const allKeys = _.union(keys1, keys2);
-
-    const allKeysSorted = _.sortBy(allKeys);
-
-    const string = allKeysSorted.reduce((acc, key) => {
-
-        if (obj1[key] === obj2[key]) {
-            // console.log('----> if keys are equal', `${acc}\n    ${key} : ${obj1[key]}`)
-            return `${acc}\n    ${key} : ${obj1[key]}`;
-        }
-
-        if (!_.has(obj1, key)) {
-            // console.log('----> if key is only in second', `${acc}\n  + ${key} : ${obj2[key]}`)
-            return `${acc}\n  + ${key} : ${obj2[key]}`;
-        }
-
-        if (!_.has(obj2, key)) {
-            // console.log('----> if key is only in first', `${acc}\n  - ${key} : ${obj1[key]}`)
-            return `${acc}\n  - ${key} : ${obj1[key]}`;
-        }
-
-        if (obj1[key] !== obj2[key]) {
-            // console.log('----> if keys are not equal', `${acc}\n  - ${key} : ${obj1[key]} \n  + ${key} : ${obj2[key]}`)
-            return `${acc}\n  - ${key} : ${obj1[key]} \n  + ${key} : ${obj2[key]}`;
-        }
-    }, '');
-
-    return `{ ${string} \n}`;
-}
+  return `{ ${string} \n}`;
+};
 
 // genDiff(path1, path2);
 
